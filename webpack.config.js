925ab5035d0e8bf6f9911e2c8be66ace70781992
nerozero1 <alex.lion.10@yandex.ru>
2021-04-20 15:56:21 +0300

    fix styles

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html'
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/assets/index.html')
     })
   ]
 }
